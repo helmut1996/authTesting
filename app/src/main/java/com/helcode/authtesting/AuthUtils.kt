9ef8eb47d0c6fcc129fbec4f47fe5f1ce9918d21
helmut1996 <josubrenes0@gmail.com>
2025-01-23 16:08:36 -0600

    subiendo proyecto

---
+++ app/src/main/java/com/helcode/authtesting/AuthUtils.kt
@@ -0,0 +1,15 @@
+package com.helcode.authtesting
+
+
+
+fun userAuthentication(email: String, password: String): Boolean {
+    if (email == "ant@gmail.com" && password == "1234"){
+        return true
+    }
+    return false
+}
+
+fun isEmailValid(email: String): Boolean {
+    val EMAIL_REGEX = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+    return EMAIL_REGEX.toRegex().matches(email);
+}
